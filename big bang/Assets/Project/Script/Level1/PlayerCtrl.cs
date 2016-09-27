56181e3620e8d9076fabd7b1f12911d52bf77fcb
Hezijian <hezijian22@gmail.com>
2016-09-27 16:26:08 +0800

    new one

--- big bang/Assets/Project/Script/Level1/PlayerCtrl.cs
+++ big bang/Assets/Project/Script/Level1/PlayerCtrl.cs
@@ -1,5 +1,4 @@
-﻿#define CONTROL_BY_JOYSTICK
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 public class PlayerCtrl : MonoBehaviour {
@@ -7,27 +6,15 @@ public class PlayerCtrl : MonoBehaviour {
 	public float speed = 6.0F;
 	public float jumpSpeed = 8.0F;
 	public float gravity = 20.0F;
-    public bool bFacingRight = true;
-    public bool bControllByMovie = false;
-
+	private Vector3 moveDirection = Vector3.zero;
 	public static  bool onLadder;    //用来判断角色是否在梯子上，与LadderCtrl脚本有交互
 
-    private Vector3 moveDirection = Vector3.zero;
-    private float JoyInputHorizontal;
-    private bool bJoyInputJump;
-    
 	private float lastTime;
 
 	void Start(){
 		lastTime=Time.realtimeSinceStartup;
 	}
 	void Update() {
-        
-        if(bControllByMovie)
-        {
-            return;
-        }
-
 		//Debug.Log(""+Input.GetAxis("Horizontal"));
 		float dtime = Time.realtimeSinceStartup-(float)lastTime;
 
@@ -35,12 +22,13 @@ public class PlayerCtrl : MonoBehaviour {
 		//保证在没跳起之前，y轴上没有速度
 		if(controller.isGrounded)
 			moveDirection.y=0;
+
 		//跳跃
 		if (controller.isGrounded||onLadder==true) {
 
-			if (bJoyInputJump || Input.GetButton("Jump")){
+			if (Input.GetButton("Jump")){
 				moveDirection.y = jumpSpeed;
-				onLadder = false;
+				onLadder=false;
 			}
 		}
 
@@ -50,15 +38,8 @@ public class PlayerCtrl : MonoBehaviour {
 		//	moveDirection.x=-speed;
 		//if(Input.GetButton("RIGHT")&&!Input.GetButton("LEFT"))
 		//	moveDirection.x=speed;
-        if(Input.GetAxis("Horizontal") > 0)
-        {
-            bFacingRight = true;
-        }
-        else if (Input.GetAxis("Horizontal") < 0)
-        {
-            bFacingRight = false;
-        }
-		moveDirection=new Vector3((Input.GetAxis("Horizontal") + JoyInputHorizontal) *speed,moveDirection.y,0);
+
+		moveDirection=new Vector3(Input.GetAxis("Horizontal") *speed,moveDirection.y,0);
 
 		//梯子上移动
 		if(onLadder==true){
@@ -73,15 +54,12 @@ public class PlayerCtrl : MonoBehaviour {
 			//	moveDirection.y=0;
 			//moveDirection.y = Input.GetAxis("Vertical")*speed;
 		}
-
 		moveDirection = transform.TransformDirection(moveDirection);
-
 		if(onLadder==false){
 			moveDirection.y -= gravity * dtime;
 		}
 
 		controller.Move(moveDirection * dtime);
-
 		transform.position=new Vector3(transform.position.x,transform.position.y,0);
 
 		lastTime=Time.realtimeSinceStartup;
@@ -96,33 +74,5 @@ public class PlayerCtrl : MonoBehaviour {
 		//Debug.Log(""+controller.isGrounded);
 	}
 
-    public void JoyStick_OnMove_MoveHorizontal(Vector2 move)
-    {
-        JoyInputHorizontal = move.x;
-        if(JoyInputHorizontal > 0)
-        {
-            bFacingRight = true;
-        }
-        else if(JoyInputHorizontal < 0)
-        {
-            bFacingRight = false;
-        }
-    }
-
-    // 不再触摸控制时重置速度
-    public void JoyStick_OnTouchUp_MoveHorizontal()
-    {
-        JoyInputHorizontal = 0;
-    }
-
-    public void JumpButton_Down()
-    {
-        bJoyInputJump = true;
-    }
-
-    public void JumpButton_Up()
-    {
-        bJoyInputJump = false;
-    }
 
 }
